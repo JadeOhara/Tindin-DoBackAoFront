012e0bec328c1b4d6420a24194729148971b11ed
Jade Ohara <jade-ohara@hotmail.com>
2021-10-12 01:40:44 -0300

    api-notes  typescript

--- api-pets/index.ts
+++ api-pets/index.ts
@@ -1,31 +1,135 @@
 //forma do typescript de importar 
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid';
+import cors from 'cors'
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 const PORT = 3000
 
 //é  preciso  tipar as variáveis
 //const contatos: any[] = []
 
 //interface  obriga o dev a enviar os dados declarados
-interface IContato {
-  name: string,
-  age: number
+interface INote {
+  id: string,
+  title: string,
+  description: number
 }
 
-//const contatos: IContato[] = []
-const contatos: Array<IContato> = []
+//const contatos: INote[] = []
+const notes: Array<INote> = []
 
 app.use(express.static('www'))
 
-app.get('/users', (req, res) => {
+//list
+app.get('/notes', (req, res) => {
 
-  contatos.push({
-    name:"Jade",
-    age:29,
-  })
-  res.json(contatos)
+  res.json(notes)
   
+})
+
+//list by id
+app.get('/notes/:id', (req, res) => {
+  //console.log(req.params.id)
+  const id = req.params.id
+
+  if (!id) {
+    return res.status(400).json({ message: 'Informe o campo id' })
+  }
+
+  const note = notes.find((n) => n.id === id)
+
+  if (!note) {
+    return res.status(400).json({ message: 'Nenhuma anotação encontrada para o id informado' })
+  }
+
+  res.send(note)
+})
+
+
+//create
+app.post('/notes', (req, res) => {
+  //console.log(req.body)
+  const title = req.body.title
+  const description = req.body.description
+
+  if (!title) {
+    return res.status(400).json({ message: 'O campo titulo é obrigatório' })
+  }
+
+  if (!description) {
+    return res.status(400).json({ message: 'O campo descrição é obrigatório' })
+  }
+
+  notes.push({
+    id: uuidv4(),
+    title,
+    description
+  })
+
+  res.send({ message: 'Anotação salva com sucesso!' })
+})
+
+//update
+app.put('/notes', (req, res) => {
+  //console.log(req.body)
+  const id = req.body.id
+  const title = req.body.title
+  const description = req.body.description
+
+  if (!id) {
+    return res.status(400).json({ message: 'Informe o campo id' })
+  }
+
+  const note = notes.find((n) => n.id === id)
+
+  if (!note) {
+    return res.status(400).json({ message: 'Nenhuma anotação encontrada para o id informado' })
+  }
+
+  if (!title) {
+    return res.status(400).json({ message: 'O campo titulo é obrigatório' })
+  }
+
+  if (!description) {
+    return res.status(400).json({ message: 'O campo descrição é obrigatório' })
+  }
+
+  for (const noteObject of notes) {
+    if (noteObject.id === id) {
+      noteObject.title = title
+      noteObject.description = description
+    }
+  }
+
+  res.send({ message: 'Anotação alterada com sucesso!' })
+})
+
+
+//delete
+app.delete('/notes', (req, res) => {
+  //console.log(req.body)
+  const id = req.body.id
+
+  if (!id) {
+    return res.status(400).json({ message: 'Informe o campo id' })
+  }
+
+  const note = notes.find((n) => n.id === id)
+
+  if (!note) {
+    return res.status(400).json({ message: 'Nenhuma anotação encontrada para o id informado' })
+  }
+
+  for  (const index in notes){
+    if(notes[index].id === id){
+      notes.splice(Number(index),1)
+    }
+  }
+
+  res.send({ message: 'Anotação excluída com sucesso!' })
 })
 
 app.listen(PORT, () => {
